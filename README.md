96097230e4226558eed2cb40eea1a4b035de3402
Exerra <exerra@exerra.xyz>
2021-12-27 20:07:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+<img src="README.svg"> 
+
+This webiste is now featuring 100% more React and 100% more server side rendering. It is based on [this template](https://github.com/Nouridio/Discord-bot-website-template)
+
+## Development
+
+You will be running two processes during development but with one command:
+
+- The Miniflare server (Miniflare is a local environment for Cloudflare Workers)
+- The Remix development server
+
+```sh
+# run concurrently command which will launch both the remix server and miniflare
+$ npm run dev
+```
+
+Open up [http://127.0.0.1:8787](http://127.0.0.1:8787) and you should be ready to go!
---
+++ app/root.jsx
@@ -0,0 +1,65 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "remix";
+
+import bootstrap from "~/styles/bootstrap.min.css";
+import mainStyle from "~/styles/main.css";
+import uiKit from "~/styles/now-ui-kit.css";
+
+export let links = () => {
+  return [
+    { rel: "stylesheet", href: bootstrap },
+    { rel: "stylesheet", href: mainStyle },
+    { rel: "stylesheet", href: uiKit }
+  ];
+};
+
+export function meta() {
+  return { title: "New Remix App" };
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <nav className="navbar navbar-expand-lg bg-transparent">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <img src="menuIcon.svg" width="20px" height="20px" style={{maxWidth: "none !important"}} />
+          </button>
+          <img src="BotLogoTransparent.png" width="50" height="50" />
+          <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{marginLeft: "20px !important"}}>
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#/">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://docs.karen.exerra.xyz">Docs</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://status.exerra.xyz">Status</a>
+              </li>
+              <li className="nav-item">
+                <a href="https://github.com/Exerra/karen-bot" className="nav-link">Github</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
