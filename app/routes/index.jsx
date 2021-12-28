abe1ff1764cf7e9c708598227815375585c09a96
Exerra <exerra@exerra.xyz>
2021-12-28 21:03:59 +0200

    some more stuff

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -8,7 +8,59 @@ export default function Index() {
 	let data = useLoaderData()
 	return (
 		<div className={"container"}>
+			<div className="header">
 
+				<div className="inner-header flex">
+					<h1>Karen Bot</h1>
+				</div>
+
+
+
+				<div>
+					<svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+						 viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+						<defs>
+							<path id="gentle-wave"
+								  d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>
+						</defs>
+						<g className="parallax">
+							<use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7"/>
+							<use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)"/>
+							<use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)"/>
+							<use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff"/>
+						</g>
+					</svg>
+				</div>
+
+			</div>
+
+			<br /><br /><br /><br /><br />
+
+			<h1 className={"flex"}>Enjoy being treated nicely?</h1>
+			<p className={"flex"}>Too bad! Karen, unlike literally 97% of all Discord bots does not care about your stupid little feelings.</p>
+
+
+			<br /><br /><br /><br /><br />
+
+			<h1 className={"flex"}>Features</h1>
+			<div className="content flex">
+				<div className={"smallWidget purple"}>
+					<h1>Profile</h1>
+					<p>Cross-server profile system</p>
+				</div>
+				<div className={"smallWidget blue"}>
+					<h1>Auto</h1>
+					<p>Auto Spotify and Brew.sh embeds</p>
+				</div>
+				<div className={"smallWidget teal"}>
+					<h1>Search</h1>
+					<p>Search in various services</p>
+				</div>
+			</div>
+
+			<footer>
+				<br /><br /><br /><br /><br /><br /><br /><br /><br />
+			</footer>
 		</div>
 	);
 }
